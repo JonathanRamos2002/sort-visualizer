481ec6956d8060df08610e3206fa913193e0de41
jonathanramos <jonathaniramos360@gmail.com>
2026-07-26 00:23:11 -0400

    shuffle and distribute bars on screen

--- visual-sort.c
+++ visual-sort.c
@@ -1,23 +1,48 @@
 #include "raylib.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 #define WIDTH 900
 #define HEIGHT 600
 
-#define NUMBARS 10
+#define NUMBARS 25
 
 int bars[NUMBARS];
+
+
+// Print the bars array to stdout, shows the index and value
+void PrintBars(){
+	for(int i = 0; i < NUMBARS; i++){
+		printf("bars[%i] = %i\n", i, bars[i]);
+	}
+}
+
 
 // Initialize bars array with values
 void CreateBars(){
 	for(int i = 0; i < NUMBARS; i++){
 		bars[i] = i + 1;
-		//printf("bars[%i] = %i\n", i, bars[i]);
-	}	
+	}
 }
 
-// Scale the bars properly and draw them to the screen evenly distributed in the screen
+
+// Randomly shuffle the bars array using a generated index 
+void ShuffleBars(){
+	srand(time(NULL)); // seed the number generator using current times
+
+	for(int i = NUMBARS - 1; i > 0; i--){
+		int random_num = rand() % (i + 1); // generating random number between 0 and i
+		
+		// randomly swap 
+		int temp = bars[i];
+		bars[i] = bars[random_num];
+		bars[random_num] = temp;
+	}
+}
+
+
+// Scale the bars properly and draw them to the screen evenly distributed
 void DrawBars(){
 	for(int i = 0; i < NUMBARS; i++){
 		int bar_height_scaled = bars[i]*HEIGHT*0.75/NUMBARS;	
@@ -37,7 +62,9 @@ int main(void)
 
     SetTargetFPS(30);               
 
-    CreateBars();		    	 
+    CreateBars();
+    ShuffleBars();
+    PrintBars(); 
 
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
